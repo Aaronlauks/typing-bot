dee620c7e73955db34ebab64eeb289576f661f01
aaronlauks <aaronlauks01@gmail.com>
2021-11-16 19:56:48 +0800

    ok

--- index.js
+++ index.js
@@ -28,39 +28,40 @@ bot.on("ready", async () => {
 
 bot.on("message", message => {
     if(message.author.id == "767633990701678602"){
+      if(message.embeds[0]) console.log(message.embeds[0])
         if(message.embeds[0].description.includes("Quick-React Rank Sim - Specops")){
             message.react("✅")
-        } else if(message.embeds[0].description.includes("How Long Does It Take For The Bomb To Explode Once Planted?")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How Long Does It Take For The Bomb To Explode Once Planted?")){
           message.channel.send("40s")
-        } else if(message.embeds[0].description.includes("How much is Kevlar Replacement?")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How much is Kevlar Replacement?")){
           message.channel.send("500")
-        } else if(message.embeds[0].description.includes("How many ARs are there on")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How many ARs are there on")){
           message.channel.send("4")
-        } else if(message.embeds[0].description.includes("How Mutch Total Ammo Does A Uratio Hold?")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How Mutch Total Ammo Does A Uratio Hold?")){
           message.channel.send("40")
-        } else if(message.embeds[0].description.includes("How many PISTOLSs are there on")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How many PISTOLSs are there on")){
           message.channel.send("5")
-        } else if(message.embeds[0].description.includes("How many SMGs are there on")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How many SMGs are there on")){
           message.channel.send("3")
-        } else if(message.embeds[0].description.includes("How many SNIPERS are there on")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How many SNIPERS are there on")){
           message.channel.send("3")
-        } else if(message.embeds[0].description.includes("How Mutch Total Ammo Does A Super-90 Hold?")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How Mutch Total Ammo Does A Super-90 Hold?")){
           message.channel.send("32")
-        } else if(message.embeds[0].description.includes("How Many Body-Taps Does it Take to")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How Many Body-Taps Does it Take to")){
           message.channel.send("3")
-        } else if(message.embeds[0].description.includes("Does a P-250 One-Tap")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("Does a P-250 One-Tap")){
           message.channel.send("No")
-        } else if(message.embeds[0].description.includes("How Mutch Total Ammo Does An MPX Hold?")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How Mutch Total Ammo Does An MPX Hold?")){
           message.channel.send("120")
-        } else if(message.embeds[0].description.includes("How many Total T6 Standard Knifes Are There?")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How many Total T6 Standard Knifes Are There?")){
           message.channel.send("6")
-        } else if(message.embeds[0].description.includes("How Mutch Total Ammo Does The Dual MTX Have?")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("How Mutch Total Ammo Does The Dual MTX Have?")){
           message.channel.send("96")
-        } else if(message.embeds[0].description.includes("What is Wallbangable in Bureau?")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("What is Wallbangable in Bureau?")){
           message.channel.send("Scanners")
-        } else if(message.embeds[0].description.includes("What is Wallbangable in Raid?")){
+        } else if(message.embeds[0].description && message.embeds[0].description.includes("What is Wallbangable in Raid?")){
           message.channel.send("Garbage Cans")
-        } else if(!message.embeds[0].description.includes("Game Result")){
+        } else if(message.embeds[0].description && !message.embeds[0].description.includes("Game Result")){
           message.channel.send("error new text")
         }
     }
